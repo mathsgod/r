f9efc9c5f0519b8feded0599bc27a5de8d900fef
Raymond Chong <mathsgod@yahoo.com>
2018-12-20 17:32:50 +0800

    orm

---
+++ class/ORM.php
@@ -0,0 +1,21 @@
+<?
+namespace R;
+
+class ORM
+{
+    public $_app;
+    public function __construct($app)
+    {
+        $this->_app = $app;
+    }
+
+    public function __get($class)
+    {
+        $table = $class::_table();
+
+        $q = new DB\Query($this->_app->db, $table);
+        $q->select();
+        return $q;
+    }
+
+}
